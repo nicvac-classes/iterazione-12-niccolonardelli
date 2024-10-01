782a778866dea5844d19c3751987e2a9040fd0be
niccolonardelli <niccolo.nardelli@marconibari.edu.it>
2024-10-01 07:49:14 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,39 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+int main()
+{
+ float costomoto, risparmi, aggiunta, n;
+ cout << "quanto costa la moto?" << endl;
+ cin >> costomoto;
+ n=0;
+ while (risparmi<costomoto)
+ {
+    cout << "inserire nuovi risparmi" << endl;
+    cin >> aggiunta;
+    risparmi = risparmi + aggiunta;
+    n=n+1;
+    if (risparmi<costomoto)
+    {
+        cout << "in questo momento hai " << risparmi << "€" << endl;
+        cout << "per poter comprare la moto ti servono ancora " << costomoto-risparmi <<"€";
+    }
+    else
+    {
+        cout << "in questo momento hai " << risparmi << "€" << endl;
+        cout << "puoi comprare la moto" << endl;
+    }
+ }
+ cout << "hai risparmiato denaro " << n << " volte" << endl;
+ cout << "hai risparmiato un totale di " << risparmi << "€" << endl;
+ if (risparmi-costomoto>0)
+ {
+    cout <<"dopo l'acquisto della moto avrai ancora " << risparmi-costomoto << "€" << endl;
+ }
+ else
+ {
+
+ }
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
